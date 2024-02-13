07512edae9dccd41d740fced42c37c679b922e55
Raju Malik <rajumaliktekion>
2024-02-13 22:03:31 +0530

    git ignore

--- App/Notes/episode2.js
+++ App/Notes/episode2.js
@@ -1,2 +1,3 @@
 //normal dep vs dev dependency i.e npm install -D parcel ,
 //-d stands for dev dependency this means we are using this dependency for development face
+//what is ~ / ^ ~-> it will upgrade to major version automatically
